8f3df860b2ad31fa76ffa9401ab1017cb4aece86
iLongRanger <rortiz0305@gmail.com>
2017-10-20 18:57:59 +0800

    updated database file

--- Files/Database/cims.sql
+++ Files/Database/cims.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 17, 2017 at 04:58 PM
+-- Generation Time: Oct 20, 2017 at 12:56 PM
 -- Server version: 10.1.21-MariaDB
 -- PHP Version: 7.1.1
 
@@ -69,7 +69,57 @@ INSERT INTO `departments` (`id`, `name`, `created_at`, `updated_at`, `descriptio
 (3, 'Accounting', '2017-10-09 09:21:46', '2017-10-09 10:01:17', 'Money Handlers'),
 (4, 'Handyman Repairs', '2017-10-09 09:33:33', '2017-10-09 09:33:33', 'Construction Firm'),
 (5, 'Human Resources', '2017-10-09 19:22:39', '2017-10-09 19:22:39', 'Hr Hr HR'),
-(6, 'Spa', '2017-10-15 21:48:43', '2017-10-15 21:48:43', 'Complete with the name of the Spa');
+(6, 'Spa', '2017-10-15 21:48:43', '2017-10-15 21:48:43', 'Complete with the name of the Spa'),
+(7, 'Auditing', '2017-10-18 04:33:24', '2017-10-18 04:33:24', 'Department where auditing happens'),
+(8, 'Marketing', '2017-10-18 04:37:31', '2017-10-18 04:37:31', 'People who markets the company');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employees`
+--
+
+CREATE TABLE `employees` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `photo_id` int(10) UNSIGNED DEFAULT NULL,
+  `lastname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `middlename` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `street` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `houseno` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `city` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `region` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `brgy` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `zipcode` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthdate` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthplace` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `marital_id` int(10) UNSIGNED DEFAULT NULL,
+  `aphone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `hphone` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `facebook` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ename` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `relationship` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `eaddress` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `econtact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `ealcontact` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `employeeid` int(11) NOT NULL DEFAULT '0',
+  `role_id` int(10) UNSIGNED DEFAULT NULL,
+  `started_date` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_id` int(10) UNSIGNED DEFAULT NULL,
+  `salary` double NOT NULL DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `employees`
+--
+
+INSERT INTO `employees` (`id`, `photo_id`, `lastname`, `firstname`, `middlename`, `street`, `houseno`, `city`, `region`, `brgy`, `zipcode`, `birthdate`, `birthplace`, `marital_id`, `aphone`, `phone`, `hphone`, `email`, `facebook`, `ename`, `relationship`, `eaddress`, `econtact`, `ealcontact`, `employeeid`, `role_id`, `started_date`, `department_id`, `salary`, `created_at`, `updated_at`) VALUES
+(1, 25, 'Ortiz', 'Ralp Jeff', 'Villanueva', 'Mt Elgon', 'B20 L26', 'Quezon City', 'NCR', 'Santa Monica', '1123', '09/05/1994', 'Marikina City', 1, '0905-736-3593', '0915-446-6034', '990-55-39', 'rortiz0305@gmail.com', 'facebook.com/ralp.ortiz', 'Christin Marie R Velez', 'Live in Partner', 'Novaliches Quezon City', '0905-736-3593', '0915-446-6034', 2120150391, 2, '10/20/2017', 1, 10000, '2017-10-18 10:57:38', '2017-10-19 22:13:41'),
+(2, 26, 'Pone', 'Sasa', 'Pone', 'Mt Elgon', 'B20 L26', 'Quezon City', 'NCR', 'Santa Monica', '1123', '03-05-1994', 'Marikina City', 1, '0905-736-3593', '0915-446-6034', '990-55-39', 'rortiz0305@gmail.com', 'facebook.com/ralp.ortiz', 'Christin Marie R Velez', 'Live in Partnet', 'Novaliches Quezon City', '0905-736-3593', '0915-446-6034', 2120150391, 2, '10/20/2017', 3, 30000, '2017-10-19 20:29:04', '2017-10-19 20:59:23');
 
 -- --------------------------------------------------------
 
@@ -96,7 +146,8 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (25, '2017_10_07_180342_create_roles_table', 4),
 (26, '2017_10_08_051539_create_department_table', 4),
 (28, '2017_10_09_172435_add_description_to_department', 5),
-(29, '2017_10_16_064603_create_customers_table', 6);
+(29, '2017_10_16_064603_create_customers_table', 6),
+(31, '2017_10_18_181248_create_employees_table', 7);
 
 -- --------------------------------------------------------
 
@@ -143,7 +194,22 @@ INSERT INTO `photos` (`id`, `file`, `created_at`, `updated_at`) VALUES
 (13, '1508134114856835_648775645143237_1265331074_o.jpg', '2017-10-15 22:08:34', '2017-10-15 22:08:34'),
 (14, '1508138744856835_648775645143237_1265331074_o.jpg', '2017-10-15 23:25:44', '2017-10-15 23:25:44'),
 (15, '1508139930Kv70y3fd.jpg', '2017-10-15 23:45:30', '2017-10-15 23:45:30'),
-(16, '1508140228jack.jpg', '2017-10-15 23:50:28', '2017-10-15 23:50:28');
+(16, '1508140228jack.jpg', '2017-10-15 23:50:28', '2017-10-15 23:50:28'),
+(17, '1508330683accountant.png', '2017-10-18 04:44:43', '2017-10-18 04:44:43'),
+(18, '1508330738atty.jpg', '2017-10-18 04:45:38', '2017-10-18 04:45:38'),
+(19, '1508330802hr.jpg', '2017-10-18 04:46:42', '2017-10-18 04:46:42'),
+(20, '1508330966auditor.png', '2017-10-18 04:49:26', '2017-10-18 04:49:26'),
+(21, '1508331049marketing.png', '2017-10-18 04:50:49', '2017-10-18 04:50:49'),
+(22, '1508331160carpenter.jpg', '2017-10-18 04:52:40', '2017-10-18 04:52:40'),
+(23, '150835259216145573_10202974452199396_2018802984_o.jpg', '2017-10-18 10:49:52', '2017-10-18 10:49:52'),
+(24, '1508352732856835_648775645143237_1265331074_o.jpg', '2017-10-18 10:52:12', '2017-10-18 10:52:12'),
+(25, '150835305816145573_10202974452199396_2018802984_o.jpg', '2017-10-18 10:57:38', '2017-10-18 10:57:38'),
+(26, '150847374417358705_1377274222293372_4898290067420934867_o.jpg', '2017-10-19 20:29:04', '2017-10-19 20:29:04'),
+(27, '150847389715747431_10202888150041896_5435667112254474776_n.jpg', '2017-10-19 20:31:37', '2017-10-19 20:31:37'),
+(28, '1508479060myAvatar.png', '2017-10-19 21:57:40', '2017-10-19 21:57:40'),
+(29, '1508479099myAvatar.png', '2017-10-19 21:58:19', '2017-10-19 21:58:19'),
+(30, '1508479720856835_648775645143237_1265331074_o.jpg', '2017-10-19 22:08:40', '2017-10-19 22:08:40'),
+(31, '1508479859605776.jpg', '2017-10-19 22:10:59', '2017-10-19 22:10:59');
 
 -- --------------------------------------------------------
 
@@ -164,7 +230,7 @@ CREATE TABLE `roles` (
 --
 
 INSERT INTO `roles` (`id`, `name`, `description`, `created_at`, `updated_at`) VALUES
-(1, 'Administrator', NULL, NULL, NULL),
+(1, 'Administrator', 'The Leader', NULL, '2017-10-18 09:05:16'),
 (2, 'Regular', '6  months and counting', NULL, '2017-10-09 19:05:07'),
 (3, 'Provisionary', 'Not Reqgular', '2017-10-09 18:59:39', '2017-10-09 18:59:39'),
 (4, 'Test', 'Testing', '2017-10-09 19:05:22', '2017-10-09 19:05:22');
@@ -177,10 +243,11 @@ INSERT INTO `roles` (`id`, `name`, `description`, `created_at`, `updated_at`) VA
 
 CREATE TABLE `users` (
   `id` int(10) UNSIGNED NOT NULL,
+  `employee_id` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,
   `role_id` int(10) UNSIGNED DEFAULT NULL,
   `is_active` int(11) NOT NULL DEFAULT '0',
   `department_id` int(10) DEFAULT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(250) COLLATE utf8mb4_unicode_ci NOT NULL,
   `email` varchar(130) COLLATE utf8mb4_unicode_ci NOT NULL,
   `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -193,12 +260,18 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `role_id`, `is_active`, `department_id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `photo_id`) VALUES
-(1, 1, 1, 1, 'Ralp Jeff Ortiz', 'rortiz0305@gmail.com', '$2y$10$k94P6vrtvThobfsRonoQPu9mtINGwcU/.vciZsV9ZucIDTNAsd0Ja', 'GnzhDiwiJaYSpxJJ1fXijDNj3TIGvnB38SZhFbTzcYEdi3seVRgZqqIutqJy', '2017-10-09 02:36:09', '2017-10-09 19:56:40', 11),
-(2, 1, 1, 1, 'Christine Marie R. Velez', 'christine@yahoo.com', '$2y$10$b0L8QLoipm4TdBEcKm/KE.kw.QvzWRWWelRoWMHZUYyUwpYtKL0Ai', '42gCsvyrlLO897BScJ8bunZ10GENiDxvyg6bEVNkaibCzbSorRnpTrSNOLH9', '2017-10-09 07:18:31', '2017-10-09 07:18:31', 5),
-(3, 1, 1, 5, 'Human Resources', 'hr@human.com', '$2y$10$nV/UyaVDRvU22hKCvFhc0O6nNm8kJT6HW1eDU/GrWSq82E7GxWVbi', 'zQu64xuRdcIWLvusJJIowtumez0lKqNVOR3ejuckk06TstebiJo9wofVRHZL', '2017-10-09 19:23:21', '2017-10-09 19:23:21', 9),
-(4, 1, 1, 2, 'Atty. Legal', 'legal@legal.com', '$2y$10$g24BH34hcAOO28onK4GF.OESMOyHn4ILvwFX7HYCDlib9ul86dO2W', 'MDcVfnLwkJ8oBFbHVra65mh5KuJp7zaO2838f6PABgIqA75Xtqwouy0oEhVF', '2017-10-09 19:25:56', '2017-10-09 19:25:56', 10),
-(5, 1, 1, 6, 'Spa Testing', 'spa@spa.com', '$2y$10$h2jn9vuE.rEiNBBjtpZWmejzQAdqQAOfu.A87v6mtGLbiJzt9kz9a', 'bwqQVbTkVl1sCIZMyjH6wh7pWLzOwp3vCwKKJByPGXC6BlqIRzThjpxkKvhL', '2017-10-15 21:49:22', '2017-10-15 22:08:34', 13);
+INSERT INTO `users` (`id`, `employee_id`, `role_id`, `is_active`, `department_id`, `name`, `email`, `password`, `remember_token`, `created_at`, `updated_at`, `photo_id`) VALUES
+(1, '', 1, 1, 1, 'Ralp Jeff  Ortiz', 'rortiz0305@gmail.com', '$2y$10$uaRz/b7MHnSDcFj232q8oeqvpJXvHuq9iCh3vJj7FjsS80QbyMAB2', 'q4tO5m8Mw21fn6QzNX8f7GGG4JeG19ahIRHO2zEkfFGkZpq2BMwqLcqXBckN', '2017-10-09 02:36:09', '2017-10-19 22:09:28', 11),
+(2, '', 1, 1, 1, 'Christine Marie R. Velez', 'christine@yahoo.com', '$2y$10$Jo07nzWBGkKp/sAlsGNYoeNnGv7ivpUoEHxf4jdSBgxhiVlBQIeri', '3kTZaFEDXWYiA9dnJilHDBiJMjElAmJTJQ2Q40n8Xv3gUvYhfzHoaD6S9t7m', '2017-10-09 07:18:31', '2017-10-19 22:09:59', 5),
+(3, '', 1, 1, 5, 'Human Resources Person', 'hr@human.com', '$2y$10$VvQZFJy1oGGC0QnqVDv/Qed8ova0qNG2Hazp47IHaj9atfTLT41Ai', 'GuopruQTXjrOgUAvI8QtBzxWlS9kkETrreM168eZeCWD5E8Za29zHgdErDIc', '2017-10-09 19:23:21', '2017-10-19 22:10:12', 19),
+(4, '', 1, 1, 2, 'Attorney Legal', 'legal@legal.com', '$2y$10$NQ4ThpJWFEBXveEwUZeEZu1QOq2bGENa0R4BVNopxWf2XFBuE7SsO', 'Rl2VfCbNbhAavCVYrBj2gOPtvfuXzEV5P5QKYsQLSEmvU6rFhElWj4FqVZEJ', '2017-10-09 19:25:56', '2017-10-19 22:10:32', 18),
+(5, '', 1, 1, 6, 'Theraphist', 'spa@spa.com', '$2y$10$0orGfCFpak2R.qb3EWr3pulpr6YczVCvUM9uJS4rS2LSKhfxt5vjC', 'i5333zqvQPkn4IAUhdCzc92koNDbfS0o8d6h5wDNLX2kVl5TH7tBPQaxHTCm', '2017-10-15 21:49:22', '2017-10-19 22:10:59', 31),
+(6, '', 1, 1, 3, 'Accountant Person', 'accountant@accounts.com', '$2y$10$F3KTTOY78Y3IwWUJc19WS.ri4QTdgDrLcWENuAWcAsPoGu2dXJ0.2', '7AFhZS78EnVPr8E0CYdX1eqSmSlNtQwExSvzNGh6P3dmq5Qg26hbtIYgD2lt', '2017-10-18 04:44:43', '2017-10-19 22:11:19', 17),
+(7, '', 1, 1, 7, 'Auditor Audits', 'auditor@audit.com', '$2y$10$oDeLzkp1w37mHDhdg6F4vu/MJv61gxpqjG70QFtEd2yGKCPmgI3JC', 'WjXDNQVQwwUuFezSnfalVX73Yhwlu5rhLxg5wccjiY4r6mHjkwqixxDk6VLW', '2017-10-18 04:49:26', '2017-10-19 22:11:32', 20),
+(8, '', 1, 1, 8, 'Someone who sells something', 'marketing@market.com', '$2y$10$A0xFUquoOHsX3gblKRGry.wfBJi.RUmj/T3Vdvag.IornrVWTGQ3O', 'ZWUsfSosVOBmBXngHtjru8RNb39BZ6Jl1XxCupgzyOWmpiE7x0Es1oS9LT8c', '2017-10-18 04:50:49', '2017-10-19 22:11:49', 21),
+(9, '', 1, 1, 4, 'Repair man', 'handyman@handyman.com', '$2y$10$h9hyjU5Xv8EQIQsi.BXr3ewbASKm.fFayehKg0goJVFo2GBeFRUu6', 'etjYEk3fkHPeC1iq4IjxEP9TJJp4pMnwyUxKLOOsrVFZ45rBugUFJpnloy31', '2017-10-18 04:52:40', '2017-10-19 22:12:00', 22),
+(10, '', 1, 1, 2, 'Ralp Jeff', 'sata@gmail.com', '$2y$10$QQH8qtoquvGO3wN43vhGaO2ixW77V8.VmRdGJs8Q4Q6YJojnlPJda', NULL, '2017-10-19 21:58:19', '2017-10-19 22:06:41', 29),
+(11, '', 2, 1, 1, 'asdasd', 'test@tse.com', '$2y$10$ugUqwyxOGj6mlXJHNj1msOyzqEU9HFm9PaNpMS.Y3dw3Bt4WtZ1ge', NULL, '2017-10-19 22:08:40', '2017-10-19 22:08:40', 30);
 
 --
 -- Indexes for dumped tables
@@ -216,6 +289,16 @@ ALTER TABLE `customers`
 --
 ALTER TABLE `departments`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `employees`
+--
+ALTER TABLE `employees`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `employees_photo_id_index` (`photo_id`),
+  ADD KEY `employees_marital_id_index` (`marital_id`),
+  ADD KEY `employees_role_id_index` (`role_id`),
+  ADD KEY `employees_department_id_index` (`department_id`);
 
 --
 -- Indexes for table `migrations`
@@ -263,17 +346,22 @@ ALTER TABLE `customers`
 -- AUTO_INCREMENT for table `departments`
 --
 ALTER TABLE `departments`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+--
+-- AUTO_INCREMENT for table `employees`
+--
+ALTER TABLE `employees`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `photos`
 --
 ALTER TABLE `photos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT for table `roles`
 --
@@ -283,7 +371,7 @@ ALTER TABLE `roles`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
